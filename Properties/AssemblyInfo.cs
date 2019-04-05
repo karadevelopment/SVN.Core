4aa76037b47457b2176a674b67646eebe20eabb1
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-04-05 05:04:39 +0200

    4.3.6.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("82c01693-2843-4662-8bfc-3726f9e6f548")]
-[assembly: AssemblyVersion("4.3.5.0")]
-[assembly: AssemblyFileVersion("4.3.5.0")]
+[assembly: AssemblyVersion("4.3.6.0")]
+[assembly: AssemblyFileVersion("4.3.6.0")]
---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿namespace SVN.Core
+{
+    public static class Utils
+    {
+        public static string CreateShortIdentifier(int value)
+        {
+            // 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+            // 123456789ABCDEFGHJKLMNPQRSTUVWXYZ
+            // 0123456789
+
+            var chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+            var nbase = chars.Length;
+
+            var result = string.Empty;
+
+            while (nbase <= value)
+            {
+                result = chars[value % nbase] + result;
+                value /= nbase;
+            }
+            result = chars[value % nbase] + result;
+
+            return result;
+        }
+    }
+}
