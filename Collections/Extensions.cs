34a3381d1a5ade630f2f6b00fa66e0a3d5ba3d17
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-04-21 13:43:52 +0200

    4.3.7.0

--- Collections/Extensions.cs
+++ Collections/Extensions.cs
@@ -26,6 +26,33 @@ namespace SVN.Core.Collections
             return param.ElementAt(index);
         }
 
+        private static IEnumerable<T> RandomizeAsync<T>(this IEnumerable<T> param, int index1, int index2)
+        {
+            for (var index = default(int); index <= index1 - 1; index++)
+            {
+                yield return param.ElementAt(index);
+            }
+            foreach (var value in param.ToList().GetRange(index1, index2 - index1 + 1).OrderBy(x => Guid.NewGuid()))
+            {
+                yield return value;
+            }
+            for (var index = index2 + 1; index <= param.Count() - 1; index++)
+            {
+                yield return param.ElementAt(index);
+            }
+        }
+
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> param)
+        {
+            return param.RandomizeAsync(-1, -1).ToList();
+        }
+
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> param, int index1, int index2)
+        {
+            index2 = Math.Max(index1, index2);
+            return param.RandomizeAsync(index1, index2).ToList();
+        }
+
         public static List<T> Invert<T>(this IEnumerable<T> param)
         {
             return param.Reverse().ToList();
